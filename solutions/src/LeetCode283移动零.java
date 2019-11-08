8920474395206775901950208a3f436513b0b7cf
dx <dxruojiu@163.com>
2019-11-08 23:06:19 +0800

    LeetCode

---
+++ solutions/src/LeetCode283移动零.java
@@ -0,0 +1,34 @@
+/**
+ * @author : dx
+ * @date : 2019/11/8
+ * Description :
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/move-zeroes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode283移动零 {
+    public void moveZeroes(int[] nums) {
+        for (int cur = 0, lastNotZero = 0; cur < nums.length; cur++) {
+            if (nums[cur] != 0) {
+                swap(nums, lastNotZero++, cur);
+            }
+        }
+    }
+
+    public void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
