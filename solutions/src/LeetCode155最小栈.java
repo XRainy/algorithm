5e5aed403e5f7fded16a39591623568661edc24c
dx <dxruojiu@163.com>
2019-11-20 13:27:21 +0800

    LeetCode

--- solutions/src/LeetCode155最小栈.java
+++ solutions/src/LeetCode155最小栈.java
@@ -4,6 +4,26 @@ import java.util.Stack;
  * @author : dx
  * @date : 2019/11/12
  * Description :
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ *
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * 示例:
+ *
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-stack
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class LeetCode155最小栈 {
     Stack<Integer> stackVal = new Stack();
