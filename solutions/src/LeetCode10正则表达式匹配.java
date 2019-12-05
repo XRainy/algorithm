1a6d0e36609fd85179685e45fb178ff1605f7217
dx <dxruojiu@163.com>
2019-12-05 23:04:43 +0800

    LeetCode

---
+++ solutions/src/LeetCode10正则表达式匹配.java
@@ -0,0 +1,88 @@
+/**
+ * @author : dx
+ * @date : 2019/12/5
+ * Description :
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * <p>
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * <p>
+ * 说明:
+ * <p>
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * s = "aa"
+ * p = "a"
+ * 输出: false
+ * 解释: "a" 无法匹配 "aa" 整个字符串。
+ * 示例 2:
+ * <p>
+ * 输入:
+ * s = "aa"
+ * p = "a*"
+ * 输出: true
+ * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ * 示例 3:
+ * <p>
+ * 输入:
+ * s = "ab"
+ * p = ".*"
+ * 输出: true
+ * 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ * 示例 4:
+ * <p>
+ * 输入:
+ * s = "aab"
+ * p = "c*a*b"
+ * 输出: true
+ * 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ * 示例 5:
+ * <p>
+ * 输入:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * 输出: false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/regular-expression-matching
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode10正则表达式匹配 {
+
+    public boolean isMatch(String s, String p){
+        if (p.length() <= 0) return s.length() <= 0;
+        boolean match = (s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'));
+        if (p.length() > 1 && p.charAt(1) == '*'){
+            return isMatch(s, p.substring(2)) || (match && isMatch(s.substring(1), p));
+        } else {
+            return match && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+    public static boolean isMatch1(String text, String pattern) {
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        dp[text.length()][pattern.length()] = true;
+        for (int i = text.length(); i >= 0; i--) {
+            for (int j = pattern.length() - 1; j >= 0; j--) {
+                boolean first_match = (i < text.length() &&
+                        (pattern.charAt(j) == text.charAt(i) ||
+                                pattern.charAt(j) == '.'));
+                if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+                    dp[i][j] = dp[i][j + 2] || first_match && dp[i + 1][j];
+                } else {
+                    dp[i][j] = first_match && dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        String s = "mississippi";
+        String p = "mis*is*p*.";
+        isMatch1(s, p);
+    }
+}
