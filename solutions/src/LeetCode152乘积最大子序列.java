8fbfc76f0836c2b8ac114bacfa5f8189e7d94c7e
dx <dxruojiu@163.com>
2019-12-22 20:59:15 +0800

    LeetCode

---
+++ solutions/src/LeetCode152乘积最大子序列.java
@@ -0,0 +1,42 @@
+/**
+ * @author : dx
+ * @date : 2019/12/22
+ * Description :
+ * 给定一个整数数组 nums ，找出一个序列中乘积最大的连续子序列（该序列至少包含一个数）。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [2,3,-2,4]
+ * 输出: 6
+ * 解释: 子数组 [2,3] 有最大乘积 6。
+ * 示例 2:
+ * <p>
+ * 输入: [-2,0,-1]
+ * 输出: 0
+ * 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-product-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode152乘积最大子序列 {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] dpMax = new int[n];
+        dpMax[0] = nums[0];
+        int[] dpMin = new int[n];
+        dpMin[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < n; i++) {
+            dpMax[i] = Math.max(dpMin[i - 1] * nums[i], Math.max(dpMax[i - 1] * nums[i], nums[i]));
+            dpMin[i] = Math.min(dpMin[i - 1] * nums[i], Math.min(dpMax[i - 1] * nums[i], nums[i]));
+            max = Math.max(max, dpMax[i]);
+        }
+        return max;
+    }
+
+}
